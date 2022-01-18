799bcc16ad63b361892f777fc79c06a8d6b56c0b
AdalbertCosta <contato@adalbertocosta.com>
2022-01-18 07:51:30 -0300

    conclusão

--- selecionandoelementos.js
+++ selecionandoelementos.js
@@ -5,10 +5,10 @@
 
 //Selecionando os elementos de uma página
 
-document.getElementById('titulo')
+document.getElementById('titulo');
 // <h1 id="titulo">Miha página</h1>
 
-document.getElementbyTagName('li')
+document.getElementbyTagName('li');
 /*
 [
   <li>Projeto 1</>
@@ -27,7 +27,8 @@ document.querySelectorAll('li .opcao');
 
 //Adicionando e deletando elementos
 
-document.createElement(element) // Cria um novo elemento HTML.
-document.removeChild(element) // remove um elemento.
-document.appendChild(element) // Adiciona um elemento.
-document.replaceChild(new,old) // substitui um elemento.
+/*vdocument.createElement(element); // Cria um novo elemento HTML.
+document.removeChild(element); // remove um elemento.
+document.appendChild(element); // Adiciona um elemento.
+document.replaceChild(new,old); // substitui um elemento.
+*/
