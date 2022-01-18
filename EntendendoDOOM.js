458a444e4883c1a8fc159dfe8c8c1dc0107f660b
AdalbertCosta <contato@adalbertocosta.com>
2022-01-18 06:22:21 -0300

    conclusão

---
+++ EntendendoDOOM.js
@@ -0,0 +1,8 @@
+//O que é o DOM
+// O DOm HTML é um padrão de como acessar e modificar os elemtnos HTMl de uma página.
+
+
+//BOM vs BOM
+
+// BOM: Browser Object MOdel
+// Tudo o que está dentro do objeto window
