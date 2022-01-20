2269f23c4b542613e1075ba2a7013a0a9c6ea934
AdalbertCosta <contato@adalbertocosta.com>
2022-01-20 06:30:50 -0300

    Class List

--- TrabalhandoComEstilos.js
+++ TrabalhandoComEstilos.js
@@ -1,2 +1,25 @@
 //Element.classList
 
+// - Exemplo
+
+<div id="meu elemento" class="classe">
+ 
+</div>
+
+const meuElemento = document.getElementById("meu elemento")
+
+meuElemento.classList.add("novo-estilo");
+//adiciona a classe "meu estilo"
+
+meuElemento.classList.remove("classe")
+// Remove a classe "classe"
+
+meuElemento.classList.toggle("dark-mode")
+// Adiciona a classe "dark-mode" caso ela não faça da lista e remove ela caso faça.
+
+
+//RESULTADO
+<div id="meu-elemento" class="novo-estilo darl=k-mode">
+</div>
+
+
